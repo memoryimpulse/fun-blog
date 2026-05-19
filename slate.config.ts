79fcb9024f8446e983dbd98bba770b01fc41054c
Vault CMS User <vault-cms@example.com>
2026-05-18 23:33:56 -0400

    Blog update 5/18/2026 11:33pm: 2 file(s) changed.

--- slate.config.ts
+++ slate.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from './src/helpers/config-helper';
 export default defineConfig({
   lang: 'en-US',
   site: 'https://slate-blog-demo.vercel.app',
-  avatar: '/avatar.png',
+  avatar: '/avatar02.png',
   title: 'in memoria',
   description: 'thoughts from the threshold.',
   lastModified: true,
